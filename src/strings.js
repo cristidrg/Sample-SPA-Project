f42929584b91f9ed8300fad1813a042e16805d39
Tim Albert <timothy.m.albert@gmail.com>
2019-03-22 10:47:19 -0400

    Remove hero string

--- src/strings.js
+++ src/strings.js
@@ -1,14 +1,4 @@
 const strings = {
-    'head': {
-        'title': 'Career Outcomes Powered by Experience',
-        'pageTitle': 'Student Outcomes',
-        'copy': `Our students graduate with full resumés of real-world experience
-         closely tied to their career paths. They’ve learned how to solve problems as part of
-         a team of professionals and navigate the unexpected challengs of today’s fast-paced workplace.
-         And they’ve gained unique insights into their classroom learning — a deeper focus and understanding
-         that better prepares them for master’s and doctoral work in top graduate programs.`,
-        'cta': 'See the numbers',
-    },
     'career': {
         'title': 'Northeastern graduates are in high demand',
         'subtitle': 'The chart below shows what Northeastern graduates are doing within nine months of graduation. Data is based on the annual Destinations survey of our graduating students’ employment and graduate school plans.',
