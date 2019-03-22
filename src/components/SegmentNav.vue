9fce8e9dc672a269b89666f74a8964a6036afa3a
rkingston <r.kingston@northeastern.edu>
2019-03-22 15:37:26 -0400

    Abstract segment navigation

---
+++ src/components/SegmentNav.vue
@@ -0,0 +1,38 @@
+<script>
+  export default {
+  };
+</script>
+
+<template>
+  <nav
+    class="navigation"
+    id="chart_menu"
+    role="navigation"
+    data-navigation-handle="#chart_menu_handle"
+    data-navigation-content="#app_nav_buttons"
+  >
+    <div class="chart-menu nav bg--gray-800">
+      <div class="my--2 chart-menu__text hidden--up@d">Browse by:</div>
+      <ul class="__list mx--2@w">
+          <li class="__item">
+            <router-link class="__link" to="/outcomes" v-scroll-to="'#app_content'">Career outcomes</router-link>
+          </li>
+          <li class="__item">
+            <router-link class="__link" to="/employment-status" v-scroll-to="'#app_content'">Employment status</router-link>
+          </li>
+          <li class="__item">
+            <router-link class="__link" to="/coop-participation" v-scroll-to="'#app_content'">Co-op participation</router-link>
+          </li>
+          <li class="__item">
+            <router-link class="__link" to="/industries" v-scroll-to="'#app_content'">By industry/company</router-link>
+          </li>
+          <li class="__item">
+            <router-link class="__link" to="/graduate-outcomes" v-scroll-to="'#app_content'">By graduate school</router-link>
+          </li>
+          <li class="__item">
+            <router-link class="__link" to="/salaries" v-scroll-to="'#app_content'">Starting salaries</router-link>
+          </li>
+      </ul>
+    </div>
+  </nav>
+</template>
