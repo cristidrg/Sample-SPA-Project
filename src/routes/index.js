ec493272b223f162bed72aa48225bdce09009cb0
Provost Digital <pods@Provosts-MacBook-Pro-2.local>
2019-03-27 15:38:47 -0400

    Move navigation around, fix industries button, update api

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,4 +4,3 @@ export {default as Industries} from './Industries.vue';
 export {default as Graduate} from './Graduate.vue';
 export {default as Outcomes} from './Outcomes.vue';
 export {default as Salaries} from './Salaries.vue';
-export {default as Navigation} from './Navigation.vue';
