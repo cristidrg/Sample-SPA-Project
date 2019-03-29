8209e80bbf43d59d5cce2bcfb4ac1584bd018116
Provost Digital <pods@Provosts-MacBook-Pro-2.local>
2019-03-29 13:10:59 -0400

    Disable buttons instead of hiding them for navigation

--- src/routes/Industries.vue
+++ src/routes/Industries.vue
@@ -82,10 +82,14 @@ export default {
         </li>
       </ul>
       <div class="industry-data__companies-nav">
-        <a v-if="companyIdx != 0" v-on:click="companyIdx -= 1" class="industry-data__companies-navbtn btn --sm tc--gray-700" href="javascript:void(0)">
+        <a v-on:click="companyIdx != 0 ? companyIdx -= 1 : null" href="javascript:void(0)" 
+          :class="`industry-data__companies-navbtn btn --sm tc--gray-700 ${companyIdx != 0 ? '' : 'bg--gray-500'}`" >
           <chevron-left /><span class="sr--only">{{strings.back_button}}</span>
         </a>
-        <a v-if="companyPageSize < companiesSortedByName.length && (companyIdx + 1) * companyPageSize < companies.length" v-on:click="companyIdx += 1"  href="javascript:void(0)" class="industry-data__companies-navbtn btn --sm tc--gray-700">
+        <a v-on:click="companyPageSize < companiesSortedByName.length && (companyIdx + 1) * companyPageSize < companies.length ? companyIdx += 1 : null"  
+          href="javascript:void(0)" 
+          :class="`industry-data__companies-navbtn btn --sm tc--gray-700 
+            ${companyPageSize < companiesSortedByName.length && (companyIdx + 1) * companyPageSize < companies.length ? '' : 'bg--gray-500'}`">
           <chevron-right /><span class="sr--only">{{strings.next_button}}</span>
         </a>
       </div>
