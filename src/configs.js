cb44425812aaa3f9d8e0cf7f5b781404ac961511
Provost Digital <pods@Provosts-MacBook-Pro-2.local>
2019-04-03 15:40:32 -0400

    Use production api

--- src/configs.js
+++ src/configs.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
-const ENDPOINT = "https://dql2agy2kzgx7ascv67kfppjdm.appsync-api.us-east-1.amazonaws.com/graphql";
+const ENDPOINT = "https://7i6ffm3gkrhxnpbd5olq53nkwm.appsync-api.us-east-1.amazonaws.com/graphql";
 
 const API = axios.create({
     baseURL: ENDPOINT,
     timeout: 20000,
     headers: {
         'Content-Type': 'application/json',
-        'x-api-key': 'da2-stz4qcnsgrg6phgtgqmtg5jkoq'
+        'x-api-key': 'da2-5edjqbmorveqnlg7mou7bv65fq'
     }
 });
 
