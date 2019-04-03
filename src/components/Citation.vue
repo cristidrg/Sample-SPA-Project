dd1b3590b7ecd25dce5c180d53380c0e3ca367f7
rkingston <r.kingston@northeastern.edu>
2019-04-03 16:01:10 -0400

    Shorten text; add persistent source/citation

---
+++ src/components/Citation.vue
@@ -0,0 +1,19 @@
+<script>
+import stringData from '../strings.js'
+
+export default {
+  name: 'citation',
+  data() {
+    return {
+        strings: stringData.citation,
+    }
+  },
+};
+</script>
+
+<template>
+  <div>
+    <hr>
+    <p class="fs--sm tc--gray-700 measure--wide" v-html="strings.source"></p>
+  </div>
+</template>
