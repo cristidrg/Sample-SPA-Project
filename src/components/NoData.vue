c76acc82c6e3ded11181e295cc3167fb2e601b82
Tim Albert <timothy.m.albert@gmail.com>
2019-04-10 14:25:46 -0400

    Update language for no data

--- src/components/NoData.vue
+++ src/components/NoData.vue
@@ -2,7 +2,7 @@
   <div class="no-data pa--2 ta--c">
     <div class="measure">
       <h3>We can’t display your requested data due to privacy restrictions.</h3>
-      <p class="mb--2 mb--0@d">The major you have chosen to view has too few graduates for us to display the data without compromising privacy. We suggest you select additional, related majors to provide a greater number of graduates.</p>
+      <p class="mb--2 mb--0@d">The major you have chosen to view has too few respondents for us to display the data without compromising privacy. We suggest you select additional, related majors to provide a greater number of graduates.</p>
       <button class="btn bg--none tc--white bc--white hidden--up@d" id="no_data_filter_menu" ref="open_filter">
         Refine Filter
       </button>
